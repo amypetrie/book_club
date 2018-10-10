b808f2e4b3161ca56896a09ae66884ce87ad28fc
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-10-10 15:42:52 -0600

    Created basic layout on book show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @top_reviews = @book.reviews.top_reviews
+    @low_reviews = @book.reviews.low_reviews
   end
-  
+
 end
