44002c8ffa3a6649e826dc929a33b09312926600
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-10-15 18:36:20 -0600

    finishes functionality for adding books and reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,31 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @book = Book.find(params[:book_id])
+    @user = User.new
+    @review = Review.new
+  end
+
+  def create
+    reviewed_book = Book.find(params[:book_id])
+    user_name = review_user_params[:user]
+    if User.find_by(user_name: user_name) == nil
+      user = User.create(user_name: user_name)
+    else
+      user = User.find_by(user_name: user_name)
+    end
+    # review_params[:user_id] user.id
+    review = reviewed_book.reviews.create(rating: review_params[:rating], review_text: review_params[:review_text], review_title:  review_params[:review_title], user_id: user.id)
+
+    redirect_to book_url(reviewed_book)
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :review_text, :review_title)
+    end
+
+    def review_user_params
+      params.require(:review).permit(:user)
+    end
+end
